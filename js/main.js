4fbd3849793b28039b413c206f749a449eb5600a
Anna Tykhonenko <av.reprintseva@gmail.com>
2021-11-24 10:04:15 +0200

    finished layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-    // document.addEventListener('click', function () {
-    //     let activeButton = document.getElementById('btn1')
-    //     activeButton.classList.add('active')
-    // })
+     document.addEventListener('click', function () {
+         let activeButton = document.getElementById('btn1')
+         activeButton.classList.add('active')
+    })
 
+
+
+     /* document.querySelector('button.like').onclick = function(){
+        console.log(this)  // теперь this это кнопка
+        this.style.background = "red";
+      }*/
